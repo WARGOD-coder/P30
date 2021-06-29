a93fd31afe1845338721b7b6870b0fdd957d2c3c
WARGOD-coder <82137603+WARGOD-coder@users.noreply.github.com>
2021-06-29 06:03:42 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,37 @@
+class Block{
+    constructor(x, y, width, height) {
+      var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.image=loadImage("block.png")
+
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.v=255
+      }
+      display(){
+        if(this.body.speed <3){
+          var angle = this.body.angle;
+          var pos= this.body.position;
+          image(this.image,this.body.position.x,this.body.position.y,30,40)
+
+          push();
+          translate(pos.x, pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          pop();
+        }else{
+          this.v=this.v-5
+          push()
+           fill(255,this.v)  
+          World.remove(world,this.body)
+           pop()
+
+}
+        
+       
+        
+}
+}
